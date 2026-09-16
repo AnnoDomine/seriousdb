a799752eaa6592a7e194c6ebeed43655704d3d33
Rayan Berrabah <108599429+rayvn-42@users.noreply.github.com>
2026-09-16 22:53:33 +0100

    Feat: add GET /db/bulk endpoint (#203)
    * Added db bulk endpoint
    
    * Fixed test file typos
    
    * Fixed assertion mismatch
    
    * Updated and implemented docstrings
    
    * Fixed removed docstrings
    
    * Fixed file formatting issue

--- src/seriousdb/main.py
+++ src/seriousdb/main.py
@@ -140,6 +140,33 @@ def get_all(cache: Annotated[Cache, Depends(get_cache)]) -> dict[str, str]:
 
 
 @app.get(
+    "/db/bulk",
+    summary="Get multiple key-value pairs",
+    description=cleandoc(
+        """
+        Returns the values stored in the database under multiple requested keys.
+
+        Keys that do not exist are omitted from the response.
+        The response contains a key-value pair for each requested key that exists in the database.
+        """
+    ),
+    response_description="The requested key-value pairs requested.",
+    responses={
+        422: {
+            "description": "Some or all the requested values are not found in the database."
+        },
+        503: {"description": "The database file could not be opened and loaded."},
+    },
+)
+def get_bulk(
+    key: Annotated[list[str], Query()], cache: Annotated[Cache, Depends(get_cache)]
+) -> dict[str, str]:
+    with cache.lock:
+        db = require_db(cache).copy()
+        return {k: db[k] for k in key if k in db}
+
+
+@app.get(
     "/db/count",
     summary="Count the stored keys",
     description=cleandoc(
